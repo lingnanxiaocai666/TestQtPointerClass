1ad9e06eec419f6a00c37d2b2a0b7c9586e29c94
anhnn19 <anhnn19@viettel.com.vn>
2018-09-27 18:29:31 +0700

    Fix qInfo not defined when Qt < 5.5

--- tst_pointertest.cpp
+++ tst_pointertest.cpp
@@ -2,6 +2,13 @@
 #include <QtTest>
 #include <QPointer>
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 5, 0)
+#define qInfo       qDebug
+#define qWarning    qDebug
+#define qFatal      qDebug
+#define qCritical   qDebug
+#endif
+
 class PointerTest : public QObject
 {
     Q_OBJECT
